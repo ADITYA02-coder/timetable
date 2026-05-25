6a4ca5794af6396a549d12f8b96fa894e9d97864
Aditya Shukla <as9025524@gmail.com>
2026-05-25 14:11:26 +0530

    deployed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/api/",
+  // baseURL: import.meta.env.VITE_API_BASE_URL || "/api/",
+  baseURL: "https://timetable-backend-phur.onrender.com/api/",
 });
 
 export const generateTimetable = async (payload) => {
